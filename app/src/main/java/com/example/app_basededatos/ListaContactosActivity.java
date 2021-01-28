3ae26c1661f80e56005b0696cf2d4206d792ba11
JuanAndresCaisapanta <49683576+JuanAndresCaisapanta@users.noreply.github.com>
2021-01-28 17:21:56 -0500

    Agregue Mostrar Listado

---
+++ app/src/main/java/com/example/app_basededatos/ListaContactosActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_basededatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaContactosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contactos);
+    }
+}
--- app/src/main/java/com/example/app_basededatos/MainActivity.java
+++ app/src/main/java/com/example/app_basededatos/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.app_basededatos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -56,12 +58,12 @@ private EditText et_codigo;
         long cantidad = database.insert("Contactos", null, fila);
         if(cantidad >0){
             Toast.makeText(this, "Contacto Agregado", Toast.LENGTH_LONG).show();
+            limpiarcontroles();
         }else{
             Toast.makeText(this, "El contacto no se pudo ingresar", Toast.LENGTH_LONG).show();
         }
         database.close();
 
-        limpiarcontroles();
     }
 
     private void limpiarcontroles() {
@@ -72,16 +74,67 @@ private EditText et_codigo;
     }
 
     public void clicbuscar(View view){
-
+        PersistirDatos persistirdatos = new PersistirDatos(this, "OCTAVODB", null, 1);
+        SQLiteDatabase database = persistirdatos.getReadableDatabase();
+        String codigo = et_codigo.getText().toString();
+        Cursor cursor = database.rawQuery("SELECT Nombre, Apellido,Telefono, Correo " +
+                "FROM Contactos WHERE Codigo="+codigo,null);
+        if(cursor.moveToFirst()){
+        et_nombre.setText(cursor.getString(0));
+            et_apellido.setText(cursor.getString(1));
+            et_telefono.setText(cursor.getString(2));
+            et_correo.setText(cursor.getString(3));
+        }else{
+            Toast.makeText(this, "El contacto no existe", Toast.LENGTH_LONG).show();
+        }
+        database.close();
     }
     public void clicmodificar(View view){
+        PersistirDatos persistirdatos = new PersistirDatos(this, "OCTAVODB", null, 1);
+        SQLiteDatabase database = persistirdatos.getWritableDatabase();
+        String nombre = et_nombre.getText().toString();
+        String apellido = et_apellido.getText().toString();
+        String telefono = et_telefono.getText().toString();
+        String correo = et_correo.getText().toString();
+        String codigo= et_codigo.getText().toString();
+        ContentValues fila = new ContentValues();
+        fila.put("Nombre",nombre);
+        fila.put("Apellido",apellido);
+        fila.put("Telefono",telefono);
+        fila.put("Correo",correo);
+        fila.put("Codigo",codigo);
+        int cantidad = database.update("Contactos", fila,"Codigo="+codigo,null);
+        if(cantidad >0){
+            Toast.makeText(this, "Contacto Actualizado", Toast.LENGTH_LONG).show();
+            limpiarcontroles();
+        }else{
+            Toast.makeText(this, "El contacto no se pudo actualizar", Toast.LENGTH_LONG).show();
+        }
+        database.close();
+
 
     }
     public void cliceliminar(View view){
+        PersistirDatos persistirdatos = new PersistirDatos(this, "OCTAVODB", null, 1);
+        SQLiteDatabase database = persistirdatos.getWritableDatabase();
 
+        String codigo= et_codigo.getText().toString();
+
+
+        int cantidad = database.delete("Contactos","Codigo="+codigo,null);
+        if(cantidad >0){
+            Toast.makeText(this, "Contacto Eliminar", Toast.LENGTH_LONG).show();
+            limpiarcontroles();
+        }else{
+            Toast.makeText(this, "El contacto no se pudo eliminar", Toast.LENGTH_LONG).show();
+        }
+        database.close();
     }
     public void cliclimpiar(View view){
-limpiarcontroles();
+       limpiarcontroles();
     }
-
+    public void cliclmostrarcontactos(View view){
+        Intent intent = new Intent(this, ListaContactosActivity.class);
+        startActivity(intent);
+    }
 }
